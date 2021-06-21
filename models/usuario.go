6e7b640c61e94dac77421a7483259b32b47aa110
Diego <diego.esc.cab@gmail.com>
2021-06-21 11:56:54 -0500

    modificación de claim,tweet,usuario

--- models/usuario.go
+++ models/usuario.go
@@ -8,15 +8,15 @@ import (
 
 /*Usuario es el modelo de ususario en la BD en mongo*/
 type Usuario struct {
-	ID              primitive.ObjectID `bson:"_id, omitempty" json:"id"`
-	Nombre          string             `bson:"nombre, omitempty" json:"nombre"`
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Nombre          string             `bson:"nombre,omitempty" json:"nombre"`
 	Apellidos       string             `bson:"apellidos" json:"apellidos"`
 	FechaNacimiento time.Time          `bson:"fechaNacimiento" json:"fechaNacimiento"`
 	Email           string             `bson:"email" json:"email"`
-	Password        string             `bson:"password, omitempty" json:"password"`
-	Avatar          string             `bson:"avatar, omitempty" json:"avatar"`
+	Password        string             `bson:"password,omitempty" json:"password"`
+	Avatar          string             `bson:"avatar,omitempty" json:"avatar"`
 	Banner          string             `bson:"banner" json:"banner"`
 	Biografia       string             `bson:"biografia" json:"biografia"`
-	Ubicacion       string             `bson:"ubicacion, omitempty" json:"ubicacion"`
+	Ubicacion       string             `bson:"ubicacion,omitempty" json:"ubicacion"`
 	SitioWeb        string             `bson:"sitioweb" json:"sitioWeb"`
 }
